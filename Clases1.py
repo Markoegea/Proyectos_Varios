cb237d28263d933c91c676c13b2a217b52dae539
Marco Alejandro Egea Moreno <markoegea@gmail.com>
2021-06-12 18:53:19 -0500

    12/06/2021 (1)

--- Clases1.py
+++ Clases1.py
@@ -1,9 +1,10 @@
 class Persona:
     def __init__(self, nombre,apellido,edad):
-        print(nombre,apellido,edad)
         self.nombre=nombre
         self.apellido=apellido
         self.edad=edad
-        pass
+    def getNombre(self):
+        return self.nombre
 
 persona1= Persona('Lincoln','Kant','70')
+print(persona1.getNombre())
