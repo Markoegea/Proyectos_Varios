d8dc4550eb55b45d9f47df27898519153409d91c
Marco Alejandro Egea Moreno <73371461+Markoegea@users.noreply.github.com>
2021-06-12 18:47:34 -0500

    12/06/2021

--- Clases1.py
+++ Clases1.py
@@ -1,5 +1,9 @@
 class Persona:
     def __init__(self, nombre,apellido,edad):
         print(nombre,apellido,edad)
+        self.nombre=nombre
+        self.apellido=apellido
+        self.edad=edad
+        pass
 
 persona1= Persona('Lincoln','Kant','70')
